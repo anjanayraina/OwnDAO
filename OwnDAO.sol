074aa4be0128ce927c67e3c1dbef02401b09bcda
Anjanay Raina <anjanayraina326@gmail.com>
2022-10-10 17:28:20 +0530

    isOwner Added

--- OwnDAO.sol
+++ OwnDAO.sol
@@ -1,7 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 contract OwnDAO{
+    address payable owner;
+    constructor(){
+        owner =payable( msg.sender);
+    }
 
+    struct Members{
+        string name;
+        address walletAddress;
+        uint releaseTime;
+        uint amount;
 
+    }
+    mapping(address => Members) allMembers;
     
+    modifier isOwner(){
+        require(owner == msg.sender , "You are not the owner");
+        _;
+    }
+
+
+
+
+
+
 }
